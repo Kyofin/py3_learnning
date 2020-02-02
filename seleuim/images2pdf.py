d81cb3a21a10d9afc3f0b72636419b5682dc0273
huzekang <1040080742@qq.com>
2020-02-03 01:28:50 +0800

    增加图片文件名按数字排序

--- seleuim/images2pdf.py
+++ seleuim/images2pdf.py
@@ -1,9 +1,11 @@
 from PIL import Image
 import os
+
 '''
 使用python3
 调用Image库拼接多张图片合成pdf
 '''
+
 
 def rea(pdf_name, imagesDirPath):
     # 获取目录所有文件名
@@ -14,6 +16,9 @@ def rea(pdf_name, imagesDirPath):
         if "jpg" in x or 'png' in x or 'jpeg' in x:
             pic_name.append(x)
     pic_name.sort()
+    # 按文件名的数字进行排序 即 1.png  2.png
+    pic_name.sort(key=lambda x: int(x[:-4]))  ##文件名按数字排序
+
     new_pic = []
     for x in pic_name:
         if "jpg" in x:
@@ -55,10 +60,10 @@ def rea(pdf_name, imagesDirPath):
 
 
 if __name__ == '__main__':
-    # pdf_name = input("请输入合成PDF文件名称：")
-    # imagesDirPath = input("请输入合成的图片目录：")
-    pdf_name = 'didi'
-    imagesDirPath = '/Users/huzekang/Downloads/jvmbook'
+    pdf_name = input("请输入合成PDF文件名称：")
+    imagesDirPath = input("请输入合成的图片目录：")
+    # pdf_name = 'didi'
+    # imagesDirPath = '/Users/huzekang/Downloads/jvmbook'
     if ".pdf" in pdf_name:
         rea(pdf_name=pdf_name, imagesDirPath=imagesDirPath)
     else:
