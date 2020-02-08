441642e92d33005b9d656075b58b17db444f9d16
huzekang <1040080742@qq.com>
2020-02-08 14:25:26 +0800

    修改phoniex示例

--- phoneix_demo/phoneix_connect.py
+++ phoneix_demo/phoneix_connect.py
@@ -1,7 +1,7 @@
 import phoenixdb.cursor
 '''
 使用python3
-selenium调用chrome截当前屏
+远程连接phoneix query server服务
 '''
 database_url = 'http://129.28.191.99:8765/'
 conn = phoenixdb.connect(database_url, autocommit=True)
@@ -9,6 +9,7 @@ conn = phoenixdb.connect(database_url, autocommit=True)
 cursor = conn.cursor()
 cursor.execute("DROP TABLE users ")
 cursor.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, username VARCHAR)")
+# 如果对应PRIMARY KEY有数据就更新，没就修改
 cursor.execute("UPSERT INTO users VALUES (?, ?)", (1, 'admin'))
 cursor.execute("SELECT * FROM users")
 print(cursor.fetchall())
